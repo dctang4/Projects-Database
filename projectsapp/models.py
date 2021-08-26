f15bf8bf9e6d9b66e9f105924bab6794100ff8fd
Chun Hin D. Tang <dtang955472@gmail.com>
2021-08-26 14:58:28 -0400

    edits

--- projectsapp/models.py
+++ projectsapp/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-  project = models.CharField(max_length=25)
-  liveurl = models.CharField(max_length=50)
-  giturl = models.CharField(max_length=50)
+  project = models.CharField(max_length=50)
+  liveurl = models.CharField(max_length=75)
+  giturl = models.CharField(max_length=75)
   image = models.CharField(max_length=150)
-  description = models.CharField(max_length=75)
+  description = models.CharField(max_length=100)
 
   def __str__(self):
         return self.name
